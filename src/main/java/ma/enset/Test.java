d51137e5ea8637778c2ba0d50372df94461e616c
NassirR <rachidnassir4@gmail.com>
2023-04-23 03:03:42 +0200

    Adding a variable message with comment

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,8 +1,10 @@
 package ma.enset;
 
 public class Test{
+    /** WTF CODE COMMENT HERE */
     public static void main(String args[]){
-        System.out.println("Hellow again ");
+        String msg = "Let's have a sneak peek at Spring Boot 2.0.";
+        System.out.println(msg);
     }
     
 }
